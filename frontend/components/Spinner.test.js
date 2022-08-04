5599a029290b12d1d3756d0ec635decfbe9afd9f
jason decker <jdeck@MacBook-Pro.local>
2022-08-03 23:41:32 -0400

    Completed All MVP Tasks

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,16 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Spinner from './Spinner'
+
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+test("Please wait displays", () => {
+  render(<Spinner setSpinnerOn={true}/>)
+
+  let text = screen.queryByText(/please wait/i)
+  expect(text).toBeInTheDocument
 })
